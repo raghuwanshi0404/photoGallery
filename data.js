9370e952250550ae46763839004bb78dbacb170e
raghuwanshi0404 <jraghuwanshi0404@gmail.com>
2023-05-21 15:37:39 +0530

    photo gall ist commit

---
+++ data.js
@@ -0,0 +1,96 @@
+export const gallaryData = [
+    {
+        category: "animal",
+        path    : "images/animal/elephant.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/elephant2.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/elephants3.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/elephant4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/elephant5.jpg"
+    },
+    
+    {
+        category: "animal",
+        path    : "images/animal/parrot1.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/parrot2.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/parrot3.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/parrot4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/parrot5.webp"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel1.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel2.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel3.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+    {
+        category: "animal",
+        path    : "images/animal/squirrel4.jpg"
+    },
+
+];
